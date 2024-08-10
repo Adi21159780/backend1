39dd0323868b0115c702a9d4a3e9b9189473f076
DIPAYANPOREL <dipayanporelcllg@gmail.com>
2024-08-10 13:13:06 +0530

    1st commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
        'ENGINE': 'django.db.backends.postgresql',
        'NAME': 'postgres',
        'USER': 'postgres',
-       'PASSWORD': '123456',
+       'PASSWORD': 'P_dipayan@084',
        'HOST': 'localhost',
        'PORT': '5432',
     }
