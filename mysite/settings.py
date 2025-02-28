41b746dac594479ca670962d4001699c704d9639
Adi21159780 <160575404+Adi21159780@users.noreply.github.com>
2025-02-28 22:45:46 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,7 +115,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'login1',
         'USER': 'postgres',
-        'PASSWORD': 'Adi@2115',
+        'PASSWORD': 'Adi-2115',
         'HOST': 'localhost',
         'PORT': '5432',
     }
